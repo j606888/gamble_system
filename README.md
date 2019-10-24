2828da546481c8db29d7c886f59fbc49c2ded209
jameslee <jameslee@kdanmobile.com>
2019-10-24 13:25:31 +0800

    Add room CRUD

--- README.md
+++ README.md
@@ -8,19 +8,7 @@
 
 ## Player
 
-* Change order
-  
-  * Order by id
-
-  * Order by score ( :desc, :order)
-
-  * Order by games count
-
 ## Room
-
-* CRUD
-
-* private / public
 
 * admin
 
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,12 +11,30 @@ class RoomsController < ApplicationController
     Room.create(rooms_params)
     redirect_to rooms_path
   end
-
+  
   def show
     @room = Room.find(params[:id])
     @players = @room.players.avaliable
     @headers = @room.header_array(record_type)
     @bodys = @room.body_array(record_type)
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    @room.update(rooms_params)
+    flash[:success] = "更新成功"
+    redirect_to @room
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    flash[:success] = "刪除房間成功"
+    redirect_to rooms_path
   end
 
   private
