37d4a43297f6f24584bf8e030dd769acee5ffdf5
jameslee <jameslee@kdanmobile.com>
2019-11-13 13:06:01 +0800

    add more secret

--- app/tools/secret.rb
+++ app/tools/secret.rb
@@ -3,5 +3,17 @@ class Secret
     def line_api
       Rails.application.credentials.send(Rails.env)[:line_api]
     end
+
+    def omniauth
+      Rails.application.credentials.send(Rails.env)[:omniauth]
+    end
+
+    def database
+      Rails.application.credentials.send(Rails.env)[:database]
+    end
+
+    def sendgrid
+      Rails.application.credentials.send(Rails.env)[:sendgrid]
+    end
   end
 end
