408e768f5b33fa8615d3d934c42dd22134d6843e
JamesLee <j606888@gmail.com>
2020-01-15 23:47:52 +0800

    Fix bug

--- db/migrate/20200113135650_create_room_maps.rb
+++ db/migrate/20200113135650_create_room_maps.rb
@@ -8,6 +8,7 @@ class CreateRoomMaps < ActiveRecord::Migration[5.2]
     end
 
     LineSource.all.each do |line_source|
+      next if line_source.room.nil?
       RoomMap.create(line_source_id: line_source.id, room_id: line_source.room.id)
     end
   end
