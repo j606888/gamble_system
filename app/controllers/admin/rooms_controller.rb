ee82ef6394ab60cc81dfbb28ac7bcae19e3e9cd5
JamesLee <j606888@gmail.com>
2020-01-16 23:27:17 +0800

    show all room in admin

--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -2,4 +2,9 @@ class Admin::RoomsController < Admin::ApplicationController
   def index
     @rooms = Room.includes(:games, :players).all
   end
+
+  def show
+    @room = Room.find(params[:id])
+    @report = @room.report
+  end
 end
