bc3c3cf6f0cf26d05e88038900409e08d9686100
JamesLee <j606888@gmail.com>
2020-07-08 21:34:23 +0800

    Add view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,9 @@ class RoomsController < ApplicationController
   def index
     @rooms = Room.includes(:players).all
   end
+
+  def show
+    @room = Room.find(params[:id])
+    @report = @room.report
+  end
 end
