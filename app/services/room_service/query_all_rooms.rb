6ea96195b699b66da41b4d00927f1f1c651de0dc
jameslee <j606888@gmail.com>
2022-02-10 23:30:16 +0800

    Fix count query

--- app/services/room_service/query_all_rooms.rb
+++ app/services/room_service/query_all_rooms.rb
@@ -2,7 +2,10 @@ class RoomService::QueryAllRooms < Service
   def initialize; end
 
   def perform
-    rooms = Room.includes(:players, :games).all
+    rooms = Room.includes(:games)
+      .joins(:players)
+      .select("rooms.*", 'COUNT(players.id) AS players_count')
+      .group("rooms.id")
     rooms = rooms.sort do |r1, r2|
       r1_created_at = r1.games.last&.created_at
       r2_created_at = r2.games.last&.created_at
